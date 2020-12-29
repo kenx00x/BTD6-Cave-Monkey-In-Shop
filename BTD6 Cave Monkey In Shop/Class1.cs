ce0c3dfd1621996cf9ad25348fb1490ab8cb0fd5
kenx00x <kenx00x@gmail.com>
2020-12-29 04:44:03 +0100

    Add project files.

---
+++ BTD6 Cave Monkey In Shop/Class1.cs
@@ -0,0 +1,106 @@
+﻿using Assets.Main.Scenes;
+using Assets.Scripts.Models.Powers;
+using Assets.Scripts.Models.Profile;
+using Assets.Scripts.Models.TowerSets;
+using Assets.Scripts.Simulation.Input;
+using Assets.Scripts.Unity;
+using Assets.Scripts.Unity.UI_New.Upgrade;
+using Harmony;
+using Il2CppSystem.Collections.Generic;
+using MelonLoader;
+using System.IO;
+[assembly: MelonInfo(typeof(BTD6_Cave_Monkey_In_Shop.Class1), "Cave Monkey In Shop", "1.0.0", "kenx00x")]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+namespace BTD6_Cave_Monkey_In_Shop
+{
+    public class Class1 : MelonMod
+    {
+        public static string dir = $"{Directory.GetCurrentDirectory()}\\Mods\\CaveMonkeyInShop";
+        public static string config = $"{dir}\\config.txt";
+        public static int caveMonkeyCost = 250;
+        public override void OnApplicationStart()
+        {
+            MelonLogger.Log("Cave Monkey In Shop mod loaded");
+            Directory.CreateDirectory($"{dir}");
+            if (File.Exists(config))
+            {
+                MelonLogger.Log("Reading config file");
+                using (StreamReader sr = File.OpenText(config))
+                {
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        caveMonkeyCost = int.Parse(s.Substring(s.IndexOf(char.Parse("=")) + 1));
+                    }
+                }
+                MelonLogger.Log("Done reading");
+            }
+            else
+            {
+                MelonLogger.Log("Creating config file");
+                using (StreamWriter sw = File.CreateText(config))
+                {
+                    sw.WriteLine("CaveMonkeyCost=250");
+                }
+                MelonLogger.Log("Done Creating");
+            }
+        }
+        [HarmonyPatch(typeof(ProfileModel), "Validate")]
+        public class ProfileModel_Patch
+        {
+            [HarmonyPostfix]
+            public static void Postfix(ProfileModel __instance)
+            {
+                List<string> unlockedTowers = __instance.unlockedTowers;
+                if (unlockedTowers.Contains("CaveMonkey"))
+                {
+                    MelonLogger.Log("Cave Monkey already unlocked");
+                }
+                else
+                {
+                    MelonLogger.Log("unlocking Cave Monkey");
+                    unlockedTowers.Add("CaveMonkey");
+                }
+            }
+        }
+        [HarmonyPatch(typeof(TitleScreen), "UpdateVersion")]
+        public class TitleScreen_Patch
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                PowerModel powerModel = Game.instance.model.GetPowerWithName("CaveMonkey");
+                if (powerModel.tower.icon == null)
+                {
+                    powerModel.tower.icon = powerModel.icon;
+                }
+                powerModel.tower.cost = caveMonkeyCost;
+                powerModel.tower.towerSet = "Support";
+            }
+        }
+        [HarmonyPatch(typeof(TowerInventory), "Init")]
+        public class TowerInventory_Patch
+        {
+            [HarmonyPrefix]
+            public static bool Prefix(ref List<TowerDetailsModel> allTowersInTheGame)
+            {
+                ShopTowerDetailsModel powerDetails = new ShopTowerDetailsModel("CaveMonkey", 1, 0, 0, 0, -1, 0, null);
+                allTowersInTheGame.Add(powerDetails);
+                return true;
+            }
+        }
+        [HarmonyPatch(typeof(UpgradeScreen), "UpdateUi")]
+        public class AddShopDetails
+        {
+            [HarmonyPrefix]
+            public static bool Prefix(ref string towerId)
+            {
+                if (towerId.Contains("CaveMonkey"))
+                {
+                    towerId = "DartMonkey";
+                }
+                return true;
+            }
+        }
+    }
+}
